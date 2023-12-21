677143d2d9e7e9d8bbe311ea2111aaf202f73dff
CodexEZ <pratapsinghaswin@gmail.com>
2023-12-21 16:17:49 +0530

    added nearest monitoring station detection algorithm

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,12 +5,10 @@ import numpy as np
 class Users(models.Model):
     username = models.CharField(max_length=50,primary_key=True,null=False)
     password = models.CharField(max_length=50,null=False)
-    name = models.CharField(max_length=100,null=False)
-    email = models.EmailField(null=False,default="")
-    phone = models.IntegerField(null=False,unique=True,default=0)
-    warnings = models.BooleanField(default=False,null=False)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6,default=0.00)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6,default=0.00)
+    email = models.EmailField(default="",null=False)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,default=0.00,null=False)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,default=0.00,null=False)
+    nearestStation = models.CharField(null = False,max_length=50,default="")
 
 
 class Area(models.Model):
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,46 @@
 import json
-
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import joblib
 from django.shortcuts import render
 from rest_framework.response import  Response
 from .Serializers import *
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Users
+from sklearn.neighbors import KNeighborsClassifier
 
 
 
+
+#This view basically accepts user data, along with his location i.e latitude and longitude, fits it into KNN classifier
+#to find the nearest water monitoring station and saves it in the data base
+
+#The API format will be ->
+# {
+#   "username": "",
+#   "password": "",
+#   "email": "",
+#   "latitude": 0,
+#   "longitude": 0,
+#   "nearestStation": ""
+# }
 @api_view(['POST'])
 def createUser(request):
+    df = pd.read_csv("D:/Aswin's project/Backend/backend/api/station.csv")
+    df = df.drop("Station Code", axis=1)
+    X = df["Name"]
+    Y = df.drop("Name", axis=1)
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(Y, X)
     serializer = CreateUserSerializer(data=request.data)
+    print(serializer.fields)
     if serializer.is_valid():
+        latitude = float(serializer.validated_data["latitude"])
+        longitude = float(serializer.validated_data["longitude"])
+        location = knn.predict([[latitude,longitude]])
+        serializer.validated_data["nearestStation"] = location[0]
         user = serializer.save()
         user.save()
         return Response(serializer.data)
